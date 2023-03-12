cef2b99bf3ea6dd7aa6d87e1a76459cf97ec2a40
Jeong Seon U <jung48273@gmail.com>
2023-03-12 15:44:22 +0900

    feat: write HelloWorld.vue style

--- corona_map/src/components/HelloWorld.vue
+++ corona_map/src/components/HelloWorld.vue
@@ -91,4 +91,19 @@ defineProps(["msg"]);
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
